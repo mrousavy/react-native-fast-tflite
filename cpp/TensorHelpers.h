f5247fbd23dfacdf973c83d24c5272f5b167abae
Marc Rousavy <me@mrousavy.com>
2023-07-04 10:43:56 +0200

    Add files

---
+++ cpp/TensorHelpers.h
@@ -0,0 +1,30 @@
+//
+//  TensorHelpers.h
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 29.06.23.
+//  Copyright © 2023 mrousavy. All rights reserved.
+//
+
+#pragma once
+
+#import <jsi/jsi.h>
+#import "../../cpp/JSITypedArray.h"
+
+class TensorHelpers {
+public:
+  /**
+   Get the size of a value of the given `TFLTensorDataType`.
+   */
+  static size_t getTFLTensorDataTypeSize(TFLTensorDataType dataType);
+  /**
+   Create a pre-allocated TypedArray for the given TFLTensor.
+   */
+  static mrousavy::TypedArrayBase createJSBufferForTensor(jsi::Runtime& runtime, TFLTensor* tensor);
+  /**
+   Copies the Tensor's data into a jsi::TypedArray and correctly casts to the given type.
+   */
+  static void updateJSBuffer(jsi::Runtime& runtime, mrousavy::TypedArrayBase& jsBuffer, TFLTensor* tensor);
+
+  static jsi::Object tensorToJSObject(jsi::Runtime& runtime, TFLTensor* tensor);
+};
