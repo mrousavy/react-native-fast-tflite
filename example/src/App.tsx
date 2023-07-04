72912b5d222bd8a61505ffd3dd9e20b85bd446fd
Marc Rousavy <me@mrousavy.com>
2023-07-04 14:54:56 +0200

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-tflite';
+import { loadTensorflowModel, useTensorflowModel } from 'react-native-tflite';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
 
   return (
     <View style={styles.container}>
