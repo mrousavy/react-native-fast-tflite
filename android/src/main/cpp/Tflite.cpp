a0fb7c00e9ac2e0d0aa4f43cf91d30a876b561f2
Marc Rousavy <me@mrousavy.com>
2023-08-21 19:00:26 +0200

    Implement `fetchByteDataFromUrl`

--- android/src/main/cpp/Tflite.cpp
+++ android/src/main/cpp/Tflite.cpp
@@ -1,38 +1,69 @@
 #include <jni.h>
 #include <jsi/jsi.h>
 #include <fbjni/fbjni.h>
+#include <exception>
+#include <memory>
 
 #include "TensorflowPlugin.h"
 #include <ReactCommon/CallInvoker.h>
 #include <ReactCommon/CallInvokerHolder.h>
 
-using namespace facebook;
+namespace mrousavy {
 
-class TfliteModule: jni::JavaClass<TfliteModule> {
+using namespace facebook;
+using namespace facebook::jni;
+
+// Java Insaller
+struct TfliteModule : public jni::JavaClass<TfliteModule> {
  public:
   static constexpr auto kJavaDescriptor = "Lcom/tflite/TfliteModule;";
 
-  static bool nativeInstall(jlong runtimePtr, jni::alias_ref<facebook::react::CallInvokerHolder::javaobject> jsCallInvokerHolder) {
-      auto runtime = reinterpret_cast<jsi::Runtime*>(runtimePtr);
-      if (runtime == nullptr) {
-          // Runtime was null!
-          return false;
-      }
-      auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
+  static jboolean nativeInstall(jlong runtimePtr,
+                                jni::alias_ref <react::CallInvokerHolder::javaobject> jsCallInvokerHolder) {
+    auto runtime = reinterpret_cast<jsi::Runtime *>(runtimePtr);
+    if (runtime == nullptr) {
+      // Runtime was null!
+      return false;
+    }
+    auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
 
-      return true;
+    auto fetchByteDataFromUrl = [](std::string url) {
+      static const auto cls = javaClassStatic();
+      static const auto method = cls->getStaticMethod<jbyteArray(std::string)>("fetchByteDataFromUrl");
+
+      auto byteData = method(cls, url);
+      auto size = byteData->size();
+      auto bytes = byteData->getRegion(0, size);
+      void* data = malloc(size);
+      memcpy(data, bytes.get(), size);
+      return Buffer {
+          .data = data,
+          .size = size
+      };
+    };
+
+    try {
+      TensorflowPlugin::installToRuntime(runtime, jsCallInvoker, fetchByteDataFromUrl);
+    } catch (std::exception &exc) {
+      return false;
+    }
+
+    return true;
   }
 
   static void registerNatives() {
     javaClassStatic()->registerNatives({
-       makeNativeMethod("nativeInstall", nativeInstall);
-    });
+      makeNativeMethod("nativeInstall",
+        TfliteModule::nativeInstall),
+      });
   }
 };
 
+} // namespace mrousavy
+
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
   return facebook::jni::initialize(vm, [] {
-    TfliteModule::registerNatives();
+    mrousavy::TfliteModule::registerNatives();
   });
 }
 
--- android/src/main/java/com/tflite/TfliteModule.java
+++ android/src/main/java/com/tflite/TfliteModule.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.proguard.annotations.DoNotStrip;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -11,6 +12,10 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /** @noinspection JavaJniMissingFunction*/
 @ReactModule(name = TfliteModule.NAME)
@@ -25,6 +30,24 @@ public class TfliteModule extends ReactContextBaseJavaModule {
   @NonNull
   public String getName() {
     return NAME;
+  }
+
+  @DoNotStrip
+  public static byte[] fetchByteDataFromUrl(String url) {
+    OkHttpClient client = new OkHttpClient();
+
+    Request request = new Request.Builder().url(url).build();
+
+    try (Response response = client.newCall(request).execute()) {
+      if (response.isSuccessful() && response.body() != null) {
+        return response.body().bytes();
+      } else {
+        throw new RuntimeException("Response was not successful!");
+      }
+    } catch (Exception e) {
+      Log.e(NAME, "Failed to fetch URL " + url + "!", e);
+      return null;
+    }
   }
 
   @ReactMethod(isBlockingSynchronousMethod = true)
