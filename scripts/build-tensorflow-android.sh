73cba3638ae29311a0eba0202749604f8e6d685f
Marc Rousavy <me@mrousavy.com>
2023-08-22 10:18:29 +0200

    Update build-tensorflow-android.sh

--- scripts/build-tensorflow-android.sh
+++ scripts/build-tensorflow-android.sh
@@ -4,15 +4,21 @@
 cd tensorflow
 
 ANDROID_SDK_HOME="$ANDROID_HOME"
-ANDROID_NDK_HOME="$ANDROID_HOME/ndk/23.1.7779620"
+ANDROID_NDK_HOME="$ANDROID_HOME/ndk/21.4.7075529"
 ANDROID_NDK_API_LEVEL="26"
 ANDROID_BUILD_TOOLS_VERSION="30.0.3"
 ANDROID_SDK_API_LEVEL="30"
 
-bazel build -c opt --fat_apk_cpu=x86,x86_64,arm64-v8a,armeabi-v7a \
-  --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
-  --define=android_dexmerger_tool=d8_dexmerger \
-  --define=android_incremental_dexing_tool=d8_dexbuilder \
-  //tensorflow/lite/java:tensorflow-lite
+cmake tensorflow/lite \
+  -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake" \
+  -DANDROID_ABI=arm64-v8a \
+  -DTFLITE_ENABLE_INSTALL=ON \
+  -DTFLITE_ENABLE_GPU=ON
+
+# bazel build -c opt --fat_apk_cpu=x86,x86_64,arm64-v8a,armeabi-v7a \
+#   --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
+#   --define=android_dexmerger_tool=d8_dexmerger \
+#   --define=android_incremental_dexing_tool=d8_dexbuilder \
+#   //tensorflow/lite/java:tensorflow-lite
 
 cd ..
