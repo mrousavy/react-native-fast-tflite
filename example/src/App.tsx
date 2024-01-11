17aa362be653e47217ee34c3f5267f8c7f524ce1
Marc Rousavy <me@mrousavy.com>
2024-01-11 18:27:23 +0100

    feat: Add VisionCamera example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,72 @@
 import * as React from 'react'
 
-import { StyleSheet, View, Text, Platform } from 'react-native'
 import {
-  useTensorflowModel,
-} from 'react-native-fast-tflite'
+  StyleSheet,
+  View,
+  Text,
+  Platform,
+  ActivityIndicator,
+} from 'react-native'
+import { useTensorflowModel } from 'react-native-fast-tflite'
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from 'react-native-vision-camera'
 
 export default function App() {
-  const [result, setResult] = React.useState('')
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const device = useCameraDevice('back')
 
   const model = useTensorflowModel(
     require('../assets/object_detection_mobile_object_localizer_v1_1_default_1.tflite'),
     Platform.OS === 'ios' ? 'core-ml' : 'default'
   )
+  const actualModel = model.state === 'loaded' ? model.model : undefined
+
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      'worklet'
+      console.log(frame.toString())
+      if (actualModel == null) {
+        // model is still loading...
+        return
+      }
+
+      const data = frame.toArrayBuffer()
+      const result = actualModel.runSync([data])
+      console.log('Result: ' + result.length)
+    },
+    [actualModel]
+  )
 
   React.useEffect(() => {
-    if (model.model == null) return
+    requestPermission()
+  }, [requestPermission])
 
-    console.log(`Running Model...`)
-    const r = model.model.run([new Uint8Array([5])])
-    r.then((output) => {
-      console.log(`Successfully ran Model!`, output)
-      setResult(`${output[0]}${output[1]}${output[2]}...`)
-    })
-  }, [model.model])
+  console.log(`Model: ${model.state} (${model.model != null})`)
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      {hasPermission && device != null ? (
+        <Camera
+          device={device}
+          style={StyleSheet.absoluteFill}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      ) : (
+        <Text>No Camera available.</Text>
+      )}
+
+      {model.state === 'loading' && (
+        <ActivityIndicator size="small" color="white" />
+      )}
+
+      {model.state === 'error' && (
+        <Text>Failed to load model! {model.error}</Text>
+      )}
     </View>
   )
 }
