cc74a621c2d2240acc6bf1e53443681579ba025c
Marc Rousavy <me@mrousavy.com>
2024-01-19 13:47:11 +0100

    Update build-tensorflow-ios.sh

--- scripts/build-tensorflow-ios.sh
+++ scripts/build-tensorflow-ios.sh
@@ -7,18 +7,22 @@ echo "Did you run ./configure in ./tensorflow/ already?"
 echo "Did you run ./configure in ./tensorflow/ already?"
 echo "Did you run ./configure in ./tensorflow/ already?"
 
-# Assumes the user ran ./configure in tensorflow/ already
-cd tensorflow
+read -p "Did you run ./configure in /tensorflow/ already? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # Assumes the user ran ./configure in tensorflow/ already
+  cd tensorflow
 
-bazel build --config=ios_fat -c opt --cxxopt=--std=c++17 //tensorflow/lite/ios:TensorFlowLiteC_framework
-bazel build --config=ios_fat -c opt --cxxopt=--std=c++17 //tensorflow/lite/ios:TensorFlowLiteCCoreML_framework
+  bazel build --config=ios_fat -c opt --cxxopt=--std=c++17 //tensorflow/lite/ios:TensorFlowLiteC_framework
+  bazel build --config=ios_fat -c opt --cxxopt=--std=c++17 //tensorflow/lite/ios:TensorFlowLiteCCoreML_framework
 
-cd ..
+  cd ..
 
-cp -f -r tensorflow/bazel-bin/tensorflow/lite/ios/ ios/
+  cp -f -r tensorflow/bazel-bin/tensorflow/lite/ios/ ios/
 
-unzip -o ios/TensorFlowLiteC_framework.zip -d ios
-rm ios/TensorFlowLiteC_framework.zip
+  unzip -o ios/TensorFlowLiteC_framework.zip -d ios
+  rm ios/TensorFlowLiteC_framework.zip
 
-unzip -o ios/TensorFlowLiteCCoreML_framework.zip -d ios
-rm ios/TensorFlowLiteCCoreML_framework.zip
+  unzip -o ios/TensorFlowLiteCCoreML_framework.zip -d ios
+  rm ios/TensorFlowLiteCCoreML_framework.zip
+fi
